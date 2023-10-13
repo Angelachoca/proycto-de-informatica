cd07dea6720d6d373a2744ba9d2757ccf07bb5af
Angelachoca <147785328+Angelachoca@users.noreply.github.com>
2023-10-12 23:40:31 -0300

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,11 @@
+basic.forever(function () {
+    if (pins.digitalReadPin(DigitalPin.P0) == 1) {
+        game.addScore(1)
+        game.addLife(0)
+        music.play(music.builtinPlayableSoundEffect(soundExpression.happy), music.PlaybackMode.InBackground)
+        basic.pause(5000)
+        music.play(music.stringPlayable("C D E - - - - - ", 100), music.PlaybackMode.UntilDone)
+    } else {
+    	
+    }
+})
